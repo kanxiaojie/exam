2bd65f217544469f241dc32af44d4f58c1cd098b
kanxiaojie <kanhappyday2008@163.com>
2016-04-11 18:27:20 +0800

    路由优化

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -6,31 +6,36 @@ Route::group(['middleware' => 'web'], function(){
 
         Route::get('/', 'PagesController@home');
 
-        Route::group(['middleware' => 'admin'], function(){
+        Route::group(['middleware' => 'admin'], function() {
 
             Route::resource('teachers', 'TeacherController', ['except' => 'show']);
             Route::resource('students', 'StudentController', ['except' => 'show']);
+        });
 
-            Route::group(['middleware' => 'teacher'], function(){
+        Route::group(['middleware' => 'teacher'], function(){
 
-                Route::resource('courses', 'CourseController', ['except' => 'show']);
-                Route::get('courses/{id}', 'CourseController@show');
-                Route::resource('courseTimes', 'CourseTimeController', ['except' => 'show']);
-                Route::resource('modules', 'ModuleController', ['except' => 'show']);
-                Route::resource('exams', 'ExamController', ['except' => 'show']);
+            Route::resource('courses', 'CourseController', ['except' => 'show']);
+            Route::get('courses/{id}', 'CourseController@show');
+            Route::resource('courseTimes', 'CourseTimeController', ['except' => 'show']);
+            Route::resource('modules', 'ModuleController', ['except' => 'show']);
+            Route::resource('exams', 'ExamController', ['except' => 'show']);
 
-                Route::group(['prefix' => 'courses/{id}'], function(){
+            Route::group(['prefix' => 'courses/{id}'], function(){
 
-                    Route::get('linkStudents', 'CourseOthersController@students');
+                Route::get('linkStudents', 'CourseOthersController@students');
+                Route::post('linkStudents/link', 'CourseOthersController@studentsLink');
+                Route::post('linkStudents/unLink', 'CourseOthersController@studentsUnLink');
 
-                });
+                Route::get('linkCourseTimes', 'CourseOthersController@courseTimes');
+//                    Route::post('linkCourseTimes/link', 'CourseOthersController@courseTimesLink');
+//                    Route::post('linkCourseTimes/unLink', 'CourseOthersController@courseTimesUnLink');
 
             });
 
-            Route::group(['middleware' => 'student'], function(){
-                Route::resource('studentsCourses', 'StudentCourseController');
-            });
+        });
 
+        Route::group(['middleware' => 'student'], function(){
+            Route::resource('studentsCourses', 'StudentCourseController');
         });
 
         Route::get('changePassword', 'ChangePasswordController@index');
