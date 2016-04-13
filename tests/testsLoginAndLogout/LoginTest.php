0723827367524fb349c682f86652117af157e7bb
kanxiaojie <kanhappyday2008@163.com>
2016-04-13 16:34:47 +0800

    登录，获取数据库用户名测试

---
+++ tests/testsLoginAndLogout/LoginTest.php
@@ -0,0 +1,18 @@
+<?php
+
+class LoginTest extends TestCase
+{
+
+    public function test_login()
+    {
+        $user = factory('App\User')->make([
+            'name' => 'Ali',
+        ]);
+
+
+        $this->actingAs($user)
+            ->withSession(['foo' => 'bar'])
+            ->visit('/');
+    }
+
+}
