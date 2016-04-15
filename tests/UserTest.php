42004f41083bb268c7f466f0cfea1f5e45359681
kanxiaojie <kanhappyday2008@163.com>
2016-04-15 11:03:06 +0800

    测试用户数据是否为空

---
+++ tests/UserTest.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Role;
+use App\User;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class UserTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+
+    /**
+     *测试用户数据不为null
+     */
+    public function test_user_is_or_not_null()
+    {
+        $this->assertNotEquals(null, User::where('student_id', 'admin')->get());
+        $this->assertNotEquals(null, Role::where('id', 1)->get());
+    }
+}
+
